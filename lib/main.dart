067d39352da60ae4465c9ace481a217db62b65d0
muratahikaru <muhi_1122@icould.com>
2022-08-09 17:28:57 +0900

    camera sample update

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,120 @@
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io'; // Fileクラスを利用するためにインポート
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  // main 関数内で非同期処理を呼び出すための設定
+  WidgetsFlutterBinding.ensureInitialized();
+  // デバイスで使用可能なカメラのリストを取得
+  final cameras = await availableCameras();
+  // 利用可能なカメラのリストから特定のカメラを取得
+  final firstCamera = cameras.first;
+  runApp(MyApp(camera: firstCamera));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
+
+  final CameraDescription camera;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Camera Example',
+      theme: ThemeData(),
+      home: TakePictureScreen(camera: camera),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+/// 写真撮影画面
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    Key? key,
+    required this.camera,
+  }) : super(key: key);
 
-  final String title;
+  final CameraDescription camera;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  TakePictureScreenState createState() => TakePictureScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  File _image = File("");
-  final picker = ImagePicker();
-  var imgFlg = false;
+class TakePictureScreenState extends State<TakePictureScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
-  Future getImage() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+  @override
+  void initState() {
+    super.initState();
 
-    setState(() {
-      if (pickedFile != null) {
-        try {
-          _image = File(pickedFile.path);
-          imgFlg = true;
-        } catch (e) {
-          imgFlg = false;
-        }
-      }
-    });
+    _controller = CameraController(
+      // カメラを指定
+      widget.camera,
+      // 解像度を定義
+      ResolutionPreset.medium,
+    );
+
+    // コントローラーを初期化
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // ウィジェットが破棄されたら、コントローラーを破棄
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: imgFlg ? Image.file(_image) : Text('写真を撮ってみましょう！'),
+        child: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller);
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: getImage,
-        child: Icon(Icons.add_a_photo),
+        onPressed: () async {
+          // 写真を撮る
+          final image = await _controller.takePicture();
+          // 表示用の画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              fullscreenDialog: true,
+            ),
+          );
+        },
+        child: const Icon(Icons.camera_alt),
       ),
+    );
+  }
+}
+
+// 撮影した写真を表示する画面
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('撮れた写真')),
+      body: Center(child: Image.file(File(imagePath))),
     );
   }
 }
