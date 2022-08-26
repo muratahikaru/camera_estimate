7cdadf84555f5f3dd9e874a5c823e8ea5da596a5
muratahikaru <muhi_1122@icould.com>
2022-08-26 17:52:07 +0900

    add image_input.dart

---
+++ lib/image_input.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
