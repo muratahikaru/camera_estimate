7d781ff2f9efab2d72b8f187e374d713da847da5
muratahikaru <muhi_1122@icould.com>
2022-09-01 17:54:27 +0900

    add: image pick

--- lib/image_input.dart
+++ lib/image_input.dart
@@ -14,3 +14,46 @@ class ImageInput extends StatefulWidget {
   @override
   _ImageInputState createState() => _ImageInputState();
 }
+
+class _ImageInputState extends State<ImageInput> {
+  final picker = ImagePicker();
+  bool loading = true;
+
+  Map<int, dynamic>? keyPoints;
+  ui.Image? image;
+
+  Future<void> _takePicture() async {
+    setState(() {
+      loading = true;
+    });
+
+    final imageFile = await picker.pickImage(
+      source: ImageSource.camera
+    );
+
+    if (imageFile == null) {
+      return;
+    }
+    poseEstimation(File(imageFile.path));
+  }
+
+  Future<void> _getImageFromGallery() async {
+    setState(() {
+      loading = true;
+    });
+
+    final imageFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (imageFile == null) {
+      return;
+    }
+
+    poseEstimation(File(imageFile.path));
+  }
+
+  static Future loadModel() async {
+
+  }
+}
