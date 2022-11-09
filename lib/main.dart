0eafd45c4c708954f4a1d9ba7c59394f05db6af1
muratahikaru <muhi_1122@icould.com>
2022-11-09 17:56:35 +0900

    squat app create

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
+import 'dart:async';
+import 'package:camera/camera.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
-import 'index_screen.dart';
 
-void main() {
-  runApp(MyApp());
+List<CameraDescription> cams = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cams = await availableCameras();
+  } on CameraException catch(e) {
+    print('Error: ${e.code}\nError Message: ${e.description}');
+  }
+  runApp(const CameraApp());
 }
 
-class MyApp extends StatelessWidget {
+class CameraApp extends StatelessWidget {
+  const CameraApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    var title = 'Muscle Training Support';
     return MaterialApp(
-      title: 'SEE FOOD',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.black,
-      ),
-      home: IndexScreen(),
+      title: title,
+      home: SquatCamPage(title: title, cameras: cams),
     );
   }
+}
+
+class SquatCamPage extends StatefulWidget {
+  final String title;
+  final List<CameraDescription> cameras;
+  final bool _showDebugMsg = true;
+  const SquatCamPage({Key? key, required this.title, required this.cameras}) : super(key: key);
 }
